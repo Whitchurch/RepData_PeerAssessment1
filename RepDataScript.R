b0a0d0fc7866c3ac5888999d3e02c79c514333ea
Whitchurch Muthumani <neoblackcyptron@gmail.com>
2020-02-12 14:17:04 +0530

    Commit all the plotting scripts

--- RepDataScript.R
+++ RepDataScript.R
@@ -1,22 +1,25 @@
 library(dplyr)
-
-## Loading and preprocessing the data
+library(lattice)
+dev.off()
+###=========================== Loading and preprocessing the data
 path <- rstudioapi::getActiveDocumentContext()$path
 pathzip <- gsub("RepDataScript.R","activity.zip",path)
 pathcsv <- gsub(pathzip,"activity.csv",pathzip)
 datatoanalyze <- read.csv(unz(pathzip, pathcsv), header = TRUE)
 
-###Preprocess the data:-
+#Step1:Preprocess the data:-
 datatoanalyze$date <- as.Date(datatoanalyze$date)
 str(datatoanalyze)
-
-### Basic structure of the data:-
+#Step2:Basic structure of the data:-
 names(datatoanalyze)
 str(datatoanalyze)
 range(datatoanalyze$interval)
 
 
-### Plot the histogram of Total number of steps taken each day:-
+
+
+
+###=========================== Plot the histogram of Total number of steps taken each day:-
 histogramdata <- group_by(datatoanalyze, date)
 histogramdata <- summarise_at(histogramdata, .vars = c("steps"),sum)
 hist(histogramdata$steps, xlab = "Total Steps per day", col = "yellow", main  ="Total steps per day", ylim = c(0,40))
@@ -25,7 +28,10 @@ meanValue <- round(mean(histogramdata$steps, na.rm = TRUE), digits = 0)
 medianValue <- median(histogramdata$steps, na.rm = TRUE)
 
 
-## What is the average daily activity pattern?
+
+
+
+###=========================== What is the average daily activity pattern?
 activitypattern <- group_by(datatoanalyze,interval)
 activitypattern <- summarize_at(activitypattern, .vars = c("steps"),mean, na.rm = TRUE)
 with(activitypattern, plot(interval,steps, type = "l", col = "black", ylab = "Average Steps",main = "Average Daily Activity Pattern"))
@@ -36,15 +42,19 @@ intervalwithMaximumSteps <- activitypattern[grep(maximumSteps,activitypattern$st
 abline(v = intervalwithMaximumSteps, col = "red")
 legend("topright",legend = c(paste("Maximum:",intervalwithMaximumSteps)),col = c("red"),lty = 1)
 
-## Imputing missing values
 
-#separate dataframe into: dataframe with NA steps;  dataframe with Non-NA steps
+
+
+###=========================== Imputing missing values
+
+#Step1: separate dataframe into: dataframe with NA steps;  dataframe with Non-NA steps
 onlyNonNAData <- datatoanalyze[!is.na(datatoanalyze$steps),]
 onlyNAData <- datatoanalyze[is.na(datatoanalyze$steps),]
 
-#get intervals inside the dataframe of NA steps
+#Step2: get intervals inside the dataframe of NA steps
 listtotraverse <- unique(onlyNAData$interval)
 
+#Step 3:
 #Impute the missing values by filling in  with (means for the 5-minute intervals)
 #Do this by:
 #Go through the listtotraverse, by interval, adding in the (means for the 5-minute intervals)
@@ -58,7 +68,12 @@ for(val in listtotraverse)
 newimputedDataframe <- rbind(onlyNonNAData,onlyNAData)
 
 
-##Histogram of the total number of steps taken each day after missing values are imputed
+
+
+
+###==================Histogram of the total number of steps taken each day after missing values are imputed
+
+# Plot: The histogram with imputed data
 histogramdata1 <- group_by(newimputedDataframe, date)
 histogramdata1 <- summarise_at(histogramdata1, .vars = c("steps"),sum)
 hist(histogramdata1$steps, xlab = "Total Steps per day", col = "yellow", main  ="Total steps per day", ylim = c(0,40))
@@ -66,7 +81,8 @@ rug(histogramdata1$steps)
 meanValue <- round(mean(histogramdata1$steps, na.rm = TRUE), digits = 0)
 medianValue <- median(histogramdata1$steps, na.rm = TRUE)
 
-par(mfrow = c(1,2), xpd = TRUE)
+#Plot: Histogram of Non-imputed , imputed data side by side. (1 row and 2 columns) layout
+par(mfrow = c(1,2))
 histogramdata <- group_by(datatoanalyze, date)
 histogramdata <- summarise_at(histogramdata, .vars = c("steps"),sum)
 hist(histogramdata$steps, xlab = "Total Steps per day", col = "yellow", main  ="(before imputation)", ylim = c(0,40))
@@ -81,9 +97,37 @@ hist(histogramdata1$steps, xlab = "Total Steps per day", col = "yellow", main  =
 rug(histogramdata1$steps)
 meanValue <- round(mean(histogramdata1$steps, na.rm = TRUE), digits = 0)
 medianValue <- round(median(histogramdata1$steps, na.rm = TRUE),digits = 0)
-legend("topright",legend = c(paste("Mean:",meanValue),paste("Median:",medianValue)), bty = "n")
+legend("topright",legend = c(paste("Mean:",meanValue),paste("Median:",medianValue)),bty = "n")
 
-par(mfrow = c(1,1))
+dev.off() # reset the display device
+
+###========================== Are there differences in activity patterns between weekdays and weekends?
+newimputedDataframe$date <- as.Date(newimputedDataframe$date)
+
+#Display the days in terms of: Mon,Tue,Wed....., and list out their breakups in a table
+daysname <- (weekdays(newimputedDataframe$date,abbreviate = TRUE))
+table(daysname)
+
+#Replace : (Mon- Fri, with: Weekday), (Sat-Sun, with the name: Weekend)
+daysname <- gsub("(Sat|Sun)","Weekend",daysname)
+daysname <- gsub("(Mon|Tue|Wed|Thu|Fri)","Weekday",daysname)
+table(daysname)
+
+#Add a new column called day_type to our dataframe:
+newimputedDataframe$day_type <- as.factor(daysname)
+
+#Group data by day_type(Weekend/Weekday), then group by Interval
+newimputedDataframe <- group_by(newimputedDataframe,day_type)
+newimputedDataframe <- group_by(newimputedDataframe,interval,add=TRUE)
+
+#Summarize the activty pattern
+activitypattern1 <- summarize_at(newimputedDataframe, .vars = c("steps"),mean, na.rm = TRUE)
+
+#Create the panel plot, for activity pattern for; Weekend/ Weekday
+xyplot(steps~interval|day_type, data = activitypattern1, layout= c(1,2), type = "l")
+
+with(activitypattern1, plot(interval,steps, type = "l", col = "black", ylab = "Average Steps",main = "Average Daily Activity Pattern"))
+
 
 
 
